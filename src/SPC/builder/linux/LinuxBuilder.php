c8f76a6977ce1530a3e698cc8e9d7e2e5829b0de
Denis Kuzmenchuk <denis.kuzm@tut.by>
2025-02-21 11:08:15 +0100

    Fix systemd option for configure command

--- src/SPC/builder/linux/LinuxBuilder.php
+++ src/SPC/builder/linux/LinuxBuilder.php
@@ -165,7 +165,7 @@ class LinuxBuilder extends UnixBuilderBase
             ->exec(
                 getenv('SPC_CMD_PREFIX_PHP_CONFIGURE') . ' ' .
                 ($enable_cli ? '--enable-cli ' : '--disable-cli ') .
-                ($enable_fpm ? '--enable-fpm --with-systemd ' : '--disable-fpm ') .
+                ($enable_fpm ? '--enable-fpm --with-fpm-systemd ' : '--disable-fpm ') .
                 ($enable_embed ? '--enable-embed=static ' : '--disable-embed ') .
                 ($enable_micro ? '--enable-micro=all-static ' : '--disable-micro ') .
                 $config_file_path .
